a8ffc49c7146375218533f07e29ad9269f9540c5
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-19 07:19:54 -1200

    Grade finializer

---
+++ Lesson1/Grade.py
@@ -0,0 +1,12 @@
+#how many grade you got 
+
+marks = int(input("Enter Your Marks :"))
+
+if (90 > marks > 80):
+    print("Grade A")
+elif (80 > marks > 70):
+    print("Grade B")
+elif(70 > marks > 60 ):
+    print("Grade C")
+else:
+    print("try next year")
