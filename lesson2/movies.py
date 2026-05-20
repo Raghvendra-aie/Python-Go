ab9a7450c96406aa97fa936a15c9b27ca6342869
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-20 08:09:13 -1200

    Favorite movies

---
+++ lesson2/movies.py
@@ -0,0 +1,15 @@
+#wap the code ask user to enter 3 favorite movies and store them into list 
+
+list = []
+
+
+a = input("Enter your favorite movie1 :")
+b = input("Enter your favorite movie2 :")
+c = input("Enter your favorite movie3 :")
+
+list.append(a)
+list.append(b)
+list.append(c)
+
+
+print(list)
