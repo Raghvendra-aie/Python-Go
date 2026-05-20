79d3e1e0afce93a921d0128a1958b5cc9a0539ea
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-20 08:33:24 -1200

    count&sort

---
+++ lesson2/count&sort.py
@@ -0,0 +1,15 @@
+#WAP to count the number of students with the “A” grade in the following tuple.
+
+grade = ("C","D","A","A","B","B","A")
+
+total = grade.count("A")
+
+print("Total student who got A is", total )
+
+#Store the above values in a list & sort them from “A” to “D”
+
+grade = ["C","D","A","A","B","B","A"]
+
+grade.sort()
+
+print(grade)
