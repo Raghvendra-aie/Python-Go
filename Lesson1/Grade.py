404e5bc3838c95fb90cc5a102d97809b2229327f
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-20 07:32:38 -1200

    Grade calculater

--- Lesson1/Grade.py
+++ Lesson1/Grade.py
@@ -2,7 +2,9 @@
 
 marks = int(input("Enter Your Marks :"))
 
-if (90 > marks > 80):
+if(100 > marks > 90):
+    print("Grade A+")
+elif (90 > marks > 80):
     print("Grade A")
 elif (80 > marks > 70):
     print("Grade B")
