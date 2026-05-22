121493f4059ade867fe4b4c130e48821b2b29221
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-22 22:24:02 +0530

    Genre : Actor

---
+++ lesson2-List&Tuples/actorcount.py
@@ -0,0 +1,13 @@
+#code choose your genre and hero Ex - Gnere is Action , Name = Tom cruse
+
+
+list = []
+
+genre = input("Enter The genre :")
+
+a = input("Enter the actor name:")
+
+list.append(genre)
+list.append(a)
+
+print(list)
