de3cb4c697ffab61fdfa5a6ac5e483653d8374f1
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-26 14:54:39 +0530

    Tables

---
+++ lesson5-Loops/table-loop.py
@@ -0,0 +1,17 @@
+#print the multiplication table of a number n
+
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
+
+print()
+print("User input system")
+print()
+
+n = int(input("Enter the value:"))
+
+i = 1 
+while i <= 10:
+    print(n*i)
+    i += 1
