64c0881f043da241c8b1e52d25af80f32088e09a
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-26 14:24:15 +0530

    counting

---
+++ lesson5-Loops/basic-loop.py
@@ -0,0 +1,16 @@
+#print number from 1 to 100
+
+i = 1 
+while i <=100:
+    print(i)
+    i += 1
+
+print("Loop ended")
+
+
+#print number from 100 to 1
+print("loop started")
+i = 100
+while i >= 1:
+    print(i)
+    i -= 1
