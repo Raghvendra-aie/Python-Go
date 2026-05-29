f1c396a37c66f742e1cf1ecf68c9912cc429cb21
Raghvendra-aie <raghvendraa171@gmail.com>
2026-05-29 22:18:46 +0530

    adding

--- lesson5-Loops/For Loops/basic2-for.py
+++ lesson5-Loops/For Loops/basic2-for.py
@@ -1,1 +1,11 @@
-tup = (1,2,4,6,8,9,12,15,19,23,26)
+tup = (1,2,4,6,8,9,12,15,19,23,26,34,23,876,665,44,787,33,9098,35,12,11,234)
+print()
+
+x = int(input("Enter the above value:"))
+
+idx = 0
+for el in tup:
+    if(el == x):
+        print(x , "found at" , "Index",idx)
+        
+    idx += 1
