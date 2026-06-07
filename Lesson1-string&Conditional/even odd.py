1ee9a6c5c0554c1e262d8997755b4bd1e6968fc9
Raghvendra-aie <raghvendraa171@gmail.com>
2026-06-07 21:48:14 +0530

    new chnge

--- Lesson1-string&Conditional/even odd.py
+++ Lesson1-string&Conditional/even odd.py
@@ -1,8 +1,10 @@
 #Checking entered Number is Odd or even
-
+print("-----------checked the number is odd or even------------")
 num = int(input("Enter Number :"))
 
 if (num % 2 == 0 ):
     print("EVEN")
 else:
     print("ODD")
+
+print("Thank You")
