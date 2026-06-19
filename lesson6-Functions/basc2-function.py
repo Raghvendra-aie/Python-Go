e3d74bac212d6842b7c6e4df755a55f2fcb4f74e
Raghvendra-aie <raghvendraa171@gmail.com>
2026-06-19 13:13:28 +0530

    CARS..

---
+++ lesson6-Functions/basc2-function.py
@@ -0,0 +1,9 @@
+#WAF to print the length of a list. ( list is the parameter)
+
+nums = [4,3,6,55,44,1,2,99]
+
+def num_print(list):
+    print(len(list))
+
+num_print(nums)
+  
---
+++ lesson6-Functions/basic3-funtion.py
@@ -0,0 +1,8 @@
+#WAF to print the elements of a list in a single line. ( list is the parameter)
+
+cars = ["BMW","audi","Ferrari","Lamboghini","Mercedes","Toyota","Ford"]
+
+def name_cars(list):  #---> 🔍defining the function
+    print(list)
+
+name_cars(cars) #---> 📞calling the function
