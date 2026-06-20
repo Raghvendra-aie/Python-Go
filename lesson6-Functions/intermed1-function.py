5e10715c24eb44e82cc909d9c48e01f3a1356c8e
Raghvendra-aie <raghvendraa171@gmail.com>
2026-06-20 22:07:49 +0530

    fact

---
+++ lesson6-Functions/intermed1-function.py
@@ -0,0 +1,11 @@
+
+cars = ["BMW","audi","Ferrari","Lamboghini","Mercedes","Toyota","Ford"]
+price = ["$400k","$350k","$1M","$1.6","$670k","$320k","$230k"]
+
+def print_list(list):
+    for item in list:
+        print(item , end=" ")
+
+
+print_list(cars)
+
